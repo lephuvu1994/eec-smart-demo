2cc173838182b8143cd46a8d43a15bed8e0c139f
vule1994 <vulewenlian94@gmail.com>
2024-12-10 19:47:58 +0700

    update floor by selector

--- server/lib/floor/floor.getByHouse.js
+++ server/lib/floor/floor.getByHouse.js
@@ -1,18 +1,19 @@
+const db = require('../../models');
 const { Floor } = require('../../models');
 
 async function getFloorsByHouse(houseId) {
-    try {
-        const floors = await Floor.find({ 
-            houseId: houseId,
-            isDeleted: false 
-        }).sort({ floorNumber: 1 });
-        
-        return floors;
-    } catch (error) {
-        throw new Error(`Error getting floors by house: ${error.message}`);
-    }
+  try {
+    const floors = await db.Floor.findAll({
+      where: {
+        house_id: houseId,
+      },
+    });
+    return floors;
+  } catch (error) {
+    throw new Error(`Error getting floors by house: ${error.message}`);
+  }
 }
 
 module.exports = {
-    getFloorsByHouse,
+  getFloorsByHouse,
 };
