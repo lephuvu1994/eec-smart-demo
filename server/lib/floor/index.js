5a6106d1ea45a8b30ce20ab92019f3c5587c66e7
vule1994 <vulewenlian94@gmail.com>
2024-12-10 19:23:42 +0700

    update brain for floor

--- server/lib/floor/index.js
+++ server/lib/floor/index.js
@@ -1,7 +1,6 @@
 const { create } = require('./floor.create');
 const { get } = require('./floor.get');
 const { getAll } = require('./floor.getAll');
-const { init } = require('./floor.init');
 const { update } = require('./floor.update');
 const { destroy } = require('./floor.destroy');
 const { getFloorsByHouse } = require('./floor.getByHouse');
