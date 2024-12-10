3d5c62d5f331f57d6460d181fc95398a887f522d
vule1994 <vulewenlian94@gmail.com>
2024-12-10 20:46:08 +0700

    update get floor

--- server/lib/floor/floor.getByHouse.js
+++ server/lib/floor/floor.getByHouse.js
@@ -1,13 +1,15 @@
 const db = require('../../models');
 
 async function getFloorsByHouse(houseId) {
-  console.log('houseId', houseId);
-  try {
-    const floors = await db.Floor.findAll({
-      where: {
-        house_id: houseId,
-      },
-    });
+    try {
+        const floors = await db.Floor.findAll({
+            where: {
+                house_id: houseId,
+            },
+        });
+        const floors2 = await db.Floor.findAll();
+        console.log('floors', floors);
+        console.log('floors2', floors2);
     return floors;
   } catch (error) {
     throw new Error(`Error getting floors by house: ${error.message}`);
