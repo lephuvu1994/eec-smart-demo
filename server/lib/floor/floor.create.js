da72992e554a15ef9f6fa6489f8816811c71fdd1
vule1994 <vulewenlian94@gmail.com>
2024-12-10 20:11:31 +0700

    update floor by selector

--- server/lib/floor/floor.create.js
+++ server/lib/floor/floor.create.js
@@ -26,8 +26,10 @@ async function create(selector, floor) {
 
   floor.house_id = house.id;
   const floorCreated = await db.Floor.create(floor);
-  const floorPlain = floorCreated.get({ plain: true });
 
+  console.log('floorCreated', floorCreated);
+  const floorPlain = floorCreated.get({ plain: true });
+  console.log('floorPlain', floorPlain);
   return floorPlain;
 }
 
